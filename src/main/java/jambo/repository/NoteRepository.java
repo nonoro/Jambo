51f8883c27f3914a74d15dc70d2ff869514fc279
wormkent <wormkent611@gmail.com>
2022-12-03 01:32:34 +0900

    feat : NoteRepository에 findNotesByReceiveUser 추가
     - 받은 쪽지함을 조회하려면 findNotesById가 아니라 받는 사람의 이메일로 조회해야되기 때문에 findNotesByReceiveUser 추가

--- src/main/java/jambo/repository/NoteRepository.java
+++ src/main/java/jambo/repository/NoteRepository.java
@@ -3,6 +3,9 @@ package jambo.repository;
 import jambo.domain.user.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface NoteRepository extends JpaRepository<Note, Long> {
+    List<Note> findNotesByReceiveUser(String email); //받는 사람 이메일로 받은 쪽지함 조회
 
 }
