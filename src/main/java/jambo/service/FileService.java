e3d9a6d65745a76b769837b6e11a432eff4c3b35
nonoro <kgu1022@naver.com>
2022-12-04 22:17:24 +0900

    feat : file 업로드시 사용할 fileService 구현완료

---
+++ src/main/java/jambo/service/FileService.java
@@ -0,0 +1,42 @@
+package jambo.service;
+
+import jambo.config.WebConfig;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Service
+public class FileService {
+
+    private final WebConfig webConfig;
+
+    public String saveFile(MultipartFile file) {
+        String fileName = getFileName(file);
+
+        try {
+            file.transferTo(new File(webConfig.getFilePath() + "/" + fileName));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return fileName;
+    }
+
+    private String getFileName(MultipartFile file) {
+        return String.format("%s.%s", UUID.randomUUID().toString().substring(0, 7), getExt(file));
+    }
+
+    private String getExt(MultipartFile file) {
+        String[] split = file.getOriginalFilename().split("\\.");
+        return split[split.length - 1];
+    }
+
+    public String getUrlPath() {
+        return webConfig.getUrlPath();
+    }
+}
