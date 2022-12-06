7e9ddf0d6140b58e1384296a6e33be27d9af5d9a
wormkent <wormkent611@gmail.com>
2022-12-06 15:04:45 +0900

    feat : 쪽지 상세보기, 쪽지 삭제 기능 추가
    - 받은 쪽지함 상세보기 기능
    - 받은 쪽지 삭제하는 기능

--- src/main/java/jambo/controller/NoteController.java
+++ src/main/java/jambo/controller/NoteController.java
@@ -1,6 +1,7 @@
 package jambo.controller;
 
 import jambo.domain.user.Note;
+import jambo.domain.user.User;
 import jambo.service.NoteService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -18,14 +19,21 @@ public class NoteController {
 
     private final NoteService noteService;
 
+    /**
+     * 받은 쪽지함
+     */
     @RequestMapping("/list")
     public String list(Model model, HttpSession session){
-       String email = "yuna@naver.com";
+       String email = "yuna@naver.com";//세션에 저장된 사용자 이메일로 변경 예정
        List<Note> list = noteService.selectAll(email);
        model.addAttribute("list", list);
+        System.out.println("list = " + list);
        return "note/list";//뷰리턴
     }
 
+    /**
+     * 등록폼
+     */
     @RequestMapping("/write")
     public String write(){
         return "note/write";
@@ -35,9 +43,31 @@ public class NoteController {
      * 쪽지 전송 : 쪽지 전송이 완료되면 받은 쪽지함으로 다시 돌아간다.
      */
     @PostMapping("/insert")
-    public String insert(Note note){
-        noteService.insert(note);
-        return "redirect:/list";//url리턴 - 리다이렉트 or 포워드(모델)
+    public String insert(Note note, HttpSession session){
+        String email = "yuna@naver.com";//세션에 저장된 사용자 이메일로 변경 예정
+//        note.setSendUser(email);
+        noteService.insert(note, email);
+        System.out.println("노트인서트!!!!!!" +note);
+        return "redirect:/note/list";//url리턴 - 리다이렉트 or 포워드(모델)
     }
 
+    /**
+     * 쪽지 상세보기
+     */
+    @RequestMapping("/read")
+    public String read(Model model, Long noteId){
+        Note note = noteService.selectBy(noteId);
+        model.addAttribute("note", note);
+        return "/note/read";
+    }
+
+    /**
+     * 삭제
+     */
+    @RequestMapping("/delete")
+    public String delete(Long noteId){
+        noteService.delete(noteId);
+        System.out.println("noteId = " + noteId);
+        return "redirect:/note/list";
+    }
 }
