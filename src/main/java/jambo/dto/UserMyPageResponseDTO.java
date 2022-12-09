97053f9b7e9dc235d161693fc52c2556a4ae3f31
wormkent <wormkent611@gmail.com>
2022-12-09 15:22:59 +0900

    feat : 마이페이지 전용 DTO 생성
     - 마이페이지에서 회원정보 조회를 위한 DTO 생성

---
+++ src/main/java/jambo/dto/UserMyPageResponseDTO.java
@@ -0,0 +1,47 @@
+package jambo.dto;
+
+import jambo.domain.user.User;
+import jambo.domain.user.UserTechStack;
+import jambo.domain.user.type.MBTI;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Data
+@AllArgsConstructor
+public class UserMyPageResponseDTO {
+
+    private String mainEmotion;
+    private String nickName;
+    private String name;
+    private String email;
+    private String phone;
+    private MBTI mbti;
+    private List<UserTechStack> userTechStacks = new ArrayList<>();
+    private Integer point;
+    private Integer level;
+    private LocalDateTime joinDate;
+
+    public static UserMyPageResponseDTO from(User user) {
+        int level = user.getPoint().getTotalPoint() / 5;
+        System.out.println();
+//        int level = 5;
+
+        return new UserMyPageResponseDTO(
+                user.getMainEmotion(),
+                user.getNickName(),
+                user.getName(),
+                user.getEmail(),
+                user.getPhone(),
+                user.getMbti(),
+                user.getUserTechStacks(),
+                user.getPoint().getAvailablePoint(),
+                level,
+                user.getJoinDate()
+        );
+    }
+}
